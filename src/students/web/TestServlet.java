5d580928674092bc5fbb030a89b7d70e46eace38
Dmytro Shparyk <dmytro.shparyk@gmail.com>
2016-08-17 21:11:26 -0500

    Major update: moving project to Web, Using Tomcat

---
+++ src/students/web/TestServlet.java
@@ -0,0 +1,38 @@
+package students.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import students.logic.Group;
+import students.logic.ManagementSystem;
+
+@WebServlet("/hello")
+public class TestServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("<B>Group List</B>");
+		pw.println("<table border=1>");
+		try {
+			List<Group> l = ManagementSystem.getInstance().getGroups();
+			for (Group gr : l) {
+				pw.println("<tr>");
+				pw.println("<td>" + gr.getGroupId() + "</td>");
+				pw.println("<td>" + gr.getNameGroup() + "</td>");
+				pw.println("<td>" + gr.getCurator() + "</td>");
+				pw.println("<td>" + gr.getSpeciality() + "</td>");
+			}
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+		pw.println("</table>");
+	}
+
+}
